1237a16d817d0fa061dc0cc31e9ac1fb64f817db
Rafael Cena <rafael.cena@hotmail.com>
2022-10-08 08:24:31 -0300

    styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Button } from './components/Button';
+
 
 export function App() {  
 
   return (
     <div >
-    <h1>Hello</h1>
+     <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
     </div>
   )
 }
